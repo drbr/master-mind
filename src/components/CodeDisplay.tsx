2fc2ef2a00a5fec10d7d8ddb2ad3da0b61e8560b
Andrew Brandon <drbr@users.noreply.github.com>
2021-04-11 23:43:18 -0400

    Put each row in a well

--- src/components/CodeDisplay.tsx
+++ src/components/CodeDisplay.tsx
@@ -1,5 +1,5 @@
-import { CodeColor } from '../logic/colors';
-import { CodePeg, CodeRow, pegGradient } from './CodeDisplayStyles';
+import { CodeColor, CodeColorsDark, CodeColorsLight } from '../logic/colors';
+import { cssClass } from '../styleFunctions';
 
 type CodeDisplayProps = {
   code: [CodeColor, CodeColor, CodeColor, CodeColor];
@@ -11,4 +11,32 @@ export function CodeDisplay(props: CodeDisplayProps) {
 
 function renderPeg(color: CodeColor) {
   return <div key={color} className={CodePeg} style={pegGradient(color)}></div>;
+}
+
+const CodeRow = cssClass('CodeRow', {
+  display: 'flex',
+  flexDirection: 'row',
+  padding: '15px 20px 15px 20px',
+  marginTop: 10,
+  borderRadius: 8,
+  boxShadow: 'inset 0px 0px 10px rgb(92 52 28)',
+});
+
+const CodePeg = cssClass('CodePeg', {
+  borderRadius: '50%',
+  height: 40,
+  width: 40,
+  margin: 8,
+  filter: 'drop-shadow(0px 1px 3px #444)',
+});
+
+function pegGradient(color: CodeColor): React.CSSProperties {
+  const lightColor = CodeColorsLight[color];
+  const darkColor = CodeColorsDark[color];
+
+  // I have no idea what I'm doing with gradients. As a starting point, I used
+  // examples from: https://cssanimation.rocks/spheres/
+  return {
+    background: `radial-gradient(circle at 25px 15px, ${lightColor}, ${darkColor})`,
+  };
 }

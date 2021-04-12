01890f24874a514116d0713246aea1f75aacaeb9
Andrew Brandon <drbr@users.noreply.github.com>
2021-04-12 17:46:16 -0400

    Saturate the custom yellow, light and dark

--- src/logic/colors.tsx
+++ src/logic/colors.tsx
@@ -5,7 +5,8 @@ export type ResponseColor = 'white' | 'black';
 // The colors taken from the reference image at the darkest spot
 export const CodeColorsDark: Record<CodeColor, string> = {
   B: 'rgb(0, 38, 173)',
-  Y: 'rgb(215, 199, 0)',
+  // Y: 'rgb(215, 199, 0)',
+  Y: 'rgb(215, 208, 0)', // custom yellow to make it stand out more from white
   P: 'rgb(215, 0, 214)',
   W: 'rgb(220, 219, 219)',
   G: 'rgb(23, 170, 0)',
@@ -15,8 +16,8 @@ export const CodeColorsDark: Record<CodeColor, string> = {
 // The colors taken from the reference image at the brightest spot
 export const CodeColorsLight: Record<CodeColor, string> = {
   B: 'rgb(142, 167, 237)',
-  Y: 'rgb(245, 239, 136)',
-  // Y: 'rgb(246, 240, 15)', // custom yellow to make it stand out more from white
+  // Y: 'rgb(245, 239, 136)',
+  Y: 'rgb(246, 240, 15)', // custom yellow to make it stand out more from white
   P: 'rgb(246, 135, 243)',
   W: 'rgb(245, 245, 245)',
   G: 'rgb(167, 232, 138)',
